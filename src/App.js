73f0b6d60fab66bce8d87122d104e685640868f8
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-05-13 20:21:38 +0530

    setup retube

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-red-600 text-center font-extrabold text-4xl">
+        You Tube Clone!
+      </h1>
+    </>
   );
 }
-
 export default App;
