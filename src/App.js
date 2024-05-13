d1a6f3603f2c981edb5f0fef396a7f8fc0c388e8
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-05-13 20:26:07 +0530

    Setting up the things

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Header from "./Components/Header";
+
 const App = () => {
   return (
     <>
-      <h1 className="text-red-600 text-center font-extrabold text-4xl">
-        You Tube Clone!
-      </h1>
+      <Header/>
     </>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <>
+            <p className="text-red-500 text-3xl font-bold">ReTube</p>
+        </>
+    );    
+}
+export default Header;
