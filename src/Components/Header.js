a9d6987070186fbcbc51f558356d7efae1fbb733
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-05-27 23:45:39 +0530

    responsive header for all the screen sizes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,23 +8,23 @@ const Header = () => {
     return (
         <>
             <div className="py-3 flex items-center justify-between">
-                <div className="md:w-12 w-28 flex justify-center items-center">
-                    <img src={menu} alt="hamburger" className="md:h-5 h-3 md:ml-28 md:w-12 w-3 -ml-3 hover:cursor-pointer" />
+                <div className="xl:w-12 w-28 flex justify-center items-center">
+                    <img src={menu} alt="hamburger" className="md:h-5 h-3 lg:ml-14 md:ml-8 xl:ml-28 xl:w-12 lg:w-12 md:w-5 w-3 -ml-3 hover:cursor-pointer" />
                     <div className="flex items-center">
-                        <img src={logo} alt="YouTube" className="md:h-10 h-4 relative md:left-7 left-3" />
-                        <p className="text-white relative md:left-8 left-3 md:text-2xl text-sm font-semibold font-sans tracking-tighter">YouTube</p>
+                        <img src={logo} alt="YouTube" className="xl:h-10 lg:h-10 md:h-7 h-5 relative md:left-7 left-3" />
+                        <p className="text-white relative md:left-8 left-3 xl:text-2xl lg:text-2xl md:text-lg text-sm font-semibold font-sans tracking-tighter">YouTube</p>
                         </div>
                 </div>
                 <div className="flex">
-                    <input type="text" placeholder="Search" className="rounded-tl-full rounded-bl-full text-white bg-[#121212] border-[1px] border-gray-600 md:w-[33rem] w-44 md:h-10 h-6 placeholder: pl-3 pb-1 md:text-lg text-xs" />
-                    <div className="md:w-16 w-5 md:h-10 h-6 border-[1px] border-gray-600 rounded-tr-full rounded-br-full bg-[#FFFFFF14] hover:cursor-pointer">
-                        <img src={search} alt="search" className="md:mt-2 mt-[0.35rem] md:ml-5 ml-[0.15rem] md:w-5 w-3"/>
+                    <input type="text" placeholder="Search" className="rounded-tl-full rounded-bl-full text-white bg-[#121212] border-[1px] border-gray-600 md:w-[19rem] xl:w-[33rem] lg:w-[30rem] w-44 md:ml-20  lg:ml-36 xl:h-10 lg:h-10 md:h-8 h-6 placeholder: pl-3 pb-1 xl:text-lg lg:text-lg md:text-base text-xs" />
+                    <div className="md:w-16 w-5 xl:h-10 lg:h-10 md:h-8 h-6 border-[1px] border-gray-600 rounded-tr-full rounded-br-full bg-[#FFFFFF14] hover:cursor-pointer">
+                        <img src={search} alt="search" className="md:mt-[0.35rem] xl:mt-2 lg:mt-2 mt-[0.35rem] md:ml-5 ml-[0.15rem] md:w-5 w-3"/>
                     </div>
                 </div>
                 <div className="flex justify-between items-center">
-                    <img src={create} alt="create" className="md:block hidden md:w-10 w-4 md:mr-6 hover:cursor-pointer"/>
-                    <img src={notification} alt="notification" className="md:w-7 w-4 md:mr-6 mr-2 hover:cursor-pointer"/>
-                    <img src={user} alt="user" className="md:w-[1.85rem] w-4 md:mr-6 mr-2 hover:cursor-pointer"/>
+                    <img src={create} alt="create" className="md:block hidden xl:w-10 lg:w-10 md:w-7 w-4 md:mr-6 hover:cursor-pointer"/>
+                    <img src={notification} alt="notification" className="xl:w-7 lg:w-7 md:w-5 w-4 md:mr-6 mr-2 hover:cursor-pointer"/>
+                    <img src={user} alt="user" className="xl:w-[1.85rem] lg:w-[1.85rem] md:w-5 w-4 md:mr-6 mr-2 hover:cursor-pointer"/>
                 </div>
         </div>
         </>
