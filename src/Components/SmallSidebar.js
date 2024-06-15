347c2a5e750e4f4118da1544d5c4d2ce531c18a8
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-06-15 23:47:51 +0530

    side bar

--- src/Components/SmallSidebar.js
+++ src/Components/SmallSidebar.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import Home from '../Images/Home.png'
+import shorts from '../Images/shorts.png'
+import subscription from '../Images/subscription.png'
+import you from '../Images/you.png'
 const SmallSidebar = () => {
   return (
     <div>
-      
+      <img src={Home} alt='home' className='ml-6 mb-8 mt-3'/>
+      <img src={shorts} alt='shorts' className='ml-6 mb-8'/>
+      <img src={subscription} alt='subscription' className='ml-6 mb-8'/>
+      <img src={you} alt='you' className='ml-6 mb-8'/>
     </div>
   )
 }
