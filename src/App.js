27ee3fea384be7c2a0f647b81a609aebdd99944e
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-06-15 23:47:20 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Body from "./Components/Body";
 import Header from "./Components/Header";
 
 const App = () => {
   return (
     <div className="bg-[#212121]">
-      <Header/>
+      <Header />
+      <Body/>
     </div>
   );
 }
