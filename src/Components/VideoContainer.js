502da1c0f60917d6cfe4d32b7452b86bb8bbd0be
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-06-16 21:51:02 +0530

    setting the API

--- src/Components/VideoContainer.js
+++ src/Components/VideoContainer.js
@@ -1,6 +1,16 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { YOUTUBE_API } from '../constant'
 const VideoContainer = () => {
+  const [youtubeData, setYouTubeData] = useState(null);
+  const fetchData = async() => {
+    const data = await fetch(YOUTUBE_API);
+    const response = await data.json();
+    setYouTubeData(response);
+    console.log("youtube",youtubeData);
+  }
+  useEffect(() => {
+    fetchData();
+  },[])
   return (
     <div>
       
--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const YOUTUBE_API_KEY = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=" + API_KEY;
 const API_KEY = "AIzaSyA18QNhj2di_5PL1Mktdkem0rqi7W9rQic";
+export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=" + API_KEY;
