136c8e60bed2495f949b93fc45c91115d56b79ac
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-06-18 23:59:26 +0530

    drop shadow

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -92,13 +92,13 @@ const Video = ({ data }) => {
   }
 
   return (
-    <div className='w-[21rem] h-[21rem] -mt-2'>
+    <div className='w-[21rem] h-[21rem] shadow-xl'>
       <img src={data?.snippet?.thumbnails?.maxres?.url} alt='videoImage' className='rounded-lg w-[55rem] h-48' />
       <div className='flex mt-3'>
         <img />
         <div className='ml-12'>
           <p className='text-white w-[17.5rem] font-semibold text-lg'>{(data?.snippet?.localized?.title).length > 55 ? data?.snippet?.localized?.title.substr(0, 55) + "..." : data?.snippet?.localized?.title}</p>
-          <p className='text-[#AAAAAA] font-semibold'>{data?.snippet?.channelTitle}</p>
+          <p className='text-[#AAAAAA] font-semibold'>{data?.snippet?.channelTitle.length > 32 ?data?.snippet?.channelTitle.substr(0,32):data?.snippet?.channelTitle}</p>
           <div className='text-[#AAAAAA] font-semibold flex'>
             <p>{formatViewCount(data?.statistics?.viewCount)} Views</p>
             <p className='text-2xl -mt-3 mx-1'>.</p>
--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
 const API_KEY = "AIzaSyA18QNhj2di_5PL1Mktdkem0rqi7W9rQic";
-export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=" + API_KEY;
+export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + API_KEY;
