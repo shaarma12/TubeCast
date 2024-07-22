7105af5ef95f404c480d3db4cb51dde46790b84e
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-07-22 20:30:21 +0530

    fixing the sidebar on clicking of the video card

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { toogleBars, toogleTag } from '../utils/toggleSlice';
+import { toogleBars, toogleState, toogleTag } from '../utils/toggleSlice';
 
 const Video = ({ data }) => {
   const dispatch = useDispatch();
@@ -100,6 +100,7 @@ const Video = ({ data }) => {
     <div onClick={() => {
       dispatch(toogleTag(false));
       dispatch(toogleBars(false));
+      dispatch(toogleState(false));
     }}>
       {toggle?<Link to={`/watch?v=${data?.id}`}><div className='w-[24.2rem] h-[21rem] mb-6 hover:cursor-pointer'>
         <img src={data?.snippet?.thumbnails?.maxres?.url ? data?.snippet?.thumbnails?.maxres?.url : data?.snippet?.thumbnails?.medium?.url} alt='videoImage' className='rounded-lg w-[55rem] h-56' />
