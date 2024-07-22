1f6ae4d0ce0ba7bc5dbd5f1a1bc21cb999275d4b
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-07-22 20:23:50 +0530

    setting the watch.js

--- src/Components/Watch.js
+++ src/Components/Watch.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const Watch = () => {
+  const [searchParams] = useSearchParams();
   return (
-    <div>
-      <p>Watch Page!</p>
+    <div className='bg-[#212121]'>
+      <iframe width="800" height="450" src={"https://www.youtube.com/embed/"+searchParams.get("v")} title="YouTube video player" frameborder="0" allow="accelerometer; autoPlay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen className='ml-40 rounded-2xl mt-4 mb-7'></iframe>
     </div>
   )
 }
