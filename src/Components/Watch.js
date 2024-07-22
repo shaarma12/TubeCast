11a74fe35b52393d5e678150bc0531c6fbb85829
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-07-22 20:36:22 +0530

    enable the autoplay the video and also enable the fullscreen video

--- src/Components/Watch.js
+++ src/Components/Watch.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { useSearchParams } from 'react-router-dom'
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 const Watch = () => {
   const [searchParams] = useSearchParams();
+  const videoId = searchParams.get("v");
+  const videoSrc = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+
   return (
     <div className='bg-[#212121]'>
-      <iframe width="800" height="450" src={"https://www.youtube.com/embed/"+searchParams.get("v")} title="YouTube video player" frameborder="0" allow="accelerometer; autoPlay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen className='ml-40 rounded-2xl mt-4 mb-7'></iframe>
+      <iframe
+        width="800"
+        height="450"
+        src={videoSrc}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+        className='ml-40 rounded-2xl mt-4 mb-7'
+      ></iframe>
     </div>
-  )
+  );
 }
 
-export default Watch
+export default Watch;
