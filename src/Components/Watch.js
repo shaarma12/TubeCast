ec7b62cb4604b3384aaea5d8720402caf499930c
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-07-23 23:45:19 +0530

    adding the channelTitle && channelLogo && No of Subscribers

--- src/Components/Watch.js
+++ src/Components/Watch.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 
 const Watch = () => {
+  const data = useSelector(store => store.Data);
+  const {description,title,channelTitle,publishedAt,thumbnails } = data?.videoData?.snippet;
+  const { viewCount, likeCount, commentCount } = data?.videoData?.statistics;
   const [searchParams] = useSearchParams();
   const videoId = searchParams.get("v");
   const videoSrc = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
 
   return (
-    <div className='bg-[#212121]'>
+    <div className='bg-[#212121] ml-40 mb-40'>
       <iframe
         width="800"
         height="450"
@@ -17,8 +21,23 @@ const Watch = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
         referrerpolicy="strict-origin-when-cross-origin"
         allowfullscreen
-        className='ml-40 rounded-2xl mt-4 mb-7'
+        className='rounded-2xl mt-4 mb-7'
       ></iframe>
+      <p className='text-white text-xl font-semibold w-[51rem] -mt-4'>{title}</p>
+      <div className='flex mt-2'>
+        <div className='flex'>
+          {/* channelLogo */}
+          <img src={ thumbnails?.maxres?.url} className='w-10 h-10 rounded-full mr-3'/>
+          {/* channelName */}
+          <div className='text-white'>
+            <p className='text-md font-medium text-white -mt-[0.1rem]'>{channelTitle}</p>
+            <p className='text-[#AAAAAA] text-sm -mt-[0.15rem]'>54.9M subscribers</p>
+          </div>
+          {/* Subscribe Button */}
+          <button className='bg-[#F1F1F1] rounded-full w-[6.5rem] h-9 ml-7 font-medium mt-1'>Subscribe</button>
+        </div>
+        <div className='flex'></div>
+      </div>
     </div>
   );
 }
