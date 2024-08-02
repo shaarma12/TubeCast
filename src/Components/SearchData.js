9302af0c3abdc4f9d3ee3e6175a9340ab065b843
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-08-02 21:32:19 +0530

    calling the Search Data Suggestion API

--- src/Components/SearchData.js
+++ src/Components/SearchData.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { SUGGESTION_DATA_API } from '../constant';
+import SearchDataCard from './SearchDataCard';
 
 const SearchData = () => {
+    const [searchParams] = useSearchParams();
+    let params = searchParams.get("search_query");
+    const [searchData, setSearchData] = useState([]);
+    useEffect(() => {
+        getSearchData();
+    }, [params]);
+    const getSearchData = async() => {
+        const data = await fetch(SUGGESTION_DATA_API + params);
+        const dataJson = await data.json();
+        setSearchData(dataJson?.items);
+        console.log("DataJson", searchData);
+    }
   return (
     <div>
-      <p className='text-white'>SearchData is here</p>
+          {searchData.map((i) => {
+          return <SearchDataCard key={i?.id?.videoId} data={i}/>
+      })}
     </div>
   )
 }
