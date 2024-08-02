4e762b5d2e80dd479869bb143cfa17104b3c18ed
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-08-02 12:02:54 +0530

    Introducing Debouncing for accurate API Calls

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import search from "../Images/search.svg"
 import notification from "../Images/notification.svg"
 import create from "../Images/create.png"
 import user from "../Images/user.png"
-import {useDispatch} from "react-redux"
+import { useDispatch } from "react-redux"
 import { toogleState } from "../utils/toggleSlice"
 import { useEffect, useState } from "react"
 import { SUGGESTION_API } from "../constant"
@@ -15,21 +15,28 @@ const Header = () => {
     const [searchData, setSearchData] = useState([]);
     const dispatch = useDispatch();
     useEffect(() => {
-        getSuggestion();
+        // Debouncing to minimise the API Calls.
+        const timer = setTimeout(() => {
+            getSuggestion();
+        }, 200);
+
+        return () => {
+            clearTimeout(timer);
+        }
+
     }, [searchQuery]);
     const getSuggestion = async () => {
         try {
-            const apiData = await fetch(SUGGESTION_API+searchQuery, {
+            const apiData = await fetch(SUGGESTION_API + searchQuery, {
                 headers: {
                     'x-cors-api-key': 'temp_0527d77dd4fa8e4e43cabb1b3fea22ce',
                 }
             });
             const jsonData = await apiData.json();
-            console.log("searchQuery:- ",searchQuery);
             setSearchData(jsonData[1]);
             console.log("Suggestions", jsonData);
         }
-        catch(error) {
+        catch (error) {
             console.log("Getting Error in Fetching Suggestion API:-", error);
         }
     }
@@ -39,17 +46,17 @@ const Header = () => {
                 <div className="xl:w-12 w-28 flex justify-center items-center">
                     <img src={menu} alt="hamburger" className="md:h-5 h-3 lg:ml-14 md:ml-8 xl:ml-28 xl:w-12 lg:w-12 md:w-5 w-3 -ml-3 hover:cursor-pointer" onClick={() => {
                         dispatch(toogleState());
-                    }}/>
+                    }} />
                     <div className="flex items-center">
                         <img src={logo} alt="YouTube" className="xl:h-10 lg:h-10 md:h-7 h-5 relative md:left-7 left-3" />
                         <p className="text-white relative md:left-8 left-3 xl:text-2xl lg:text-2xl md:text-lg text-sm font-semibold font-sans tracking-tighter">TubeCast</p>
-                        </div>
+                    </div>
                 </div>
                 <div className="flex">
                     {inputFocus && <div className="flex justify-center w-6 absolute right-[65rem] h-10 bg-[#121212] border-[1px] border-gray-600 border-r-0 rounded-tl-full rounded-bl-full">
                         <img src={search} className="w-4 ml-6" />
                     </div>}
-                    {inputFocus&&<input type="text" placeholder="Search" value={searchQuery} className="outline-none border-l-0 text-white bg-[#121212] border-[1px] border-gray-600 md:w-[19rem] xl:w-[33rem] lg:w-[30rem] w-44 md:ml-20  lg:ml-36 xl:h-10 lg:h-10 md:h-8 h-6 placeholder: pl-7 pb-1 xl:text-lg lg:text-lg md:text-base text-xs" onFocus={() => {
+                    {inputFocus && <input type="text" placeholder="Search" value={searchQuery} className="outline-none border-l-0 text-white bg-[#121212] border-[1px] border-gray-600 md:w-[19rem] xl:w-[33rem] lg:w-[30rem] w-44 md:ml-20  lg:ml-36 xl:h-10 lg:h-10 md:h-8 h-6 placeholder: pl-7 pb-1 xl:text-lg lg:text-lg md:text-base text-xs" onFocus={() => {
                         setInputFocus(true);
                     }}
                         onBlur={() => {
@@ -59,7 +66,7 @@ const Header = () => {
                         onChange={(e) => {
                             setSearchQuery(e.target.value);
                         }} />}
-                    {!inputFocus&&<input type="text" placeholder="Search" value={searchQuery} className="rounded-tl-full rounded-bl-full text-white bg-[#121212] border-[1px] border-gray-600 md:w-[19rem] xl:w-[33rem] lg:w-[30rem] w-44 md:ml-20  lg:ml-36 xl:h-10 lg:h-10 md:h-8 h-6 placeholder: pl-6 pb-1 xl:text-lg lg:text-lg md:text-base text-xs" onFocus={() => {
+                    {!inputFocus && <input type="text" placeholder="Search" value={searchQuery} className="rounded-tl-full rounded-bl-full text-white bg-[#121212] border-[1px] border-gray-600 md:w-[19rem] xl:w-[33rem] lg:w-[30rem] w-44 md:ml-20  lg:ml-36 xl:h-10 lg:h-10 md:h-8 h-6 placeholder: pl-6 pb-1 xl:text-lg lg:text-lg md:text-base text-xs" onFocus={() => {
                         setInputFocus(true);
                     }}
                         onBlur={() => {
@@ -70,16 +77,16 @@ const Header = () => {
                             setSearchQuery(e.target.value);
                         }} />}
                     <div className="md:w-16 w-5 xl:h-10 lg:h-10 md:h-8 h-6 border-[1px] border-gray-600 rounded-tr-full rounded-br-full bg-[#FFFFFF14] hover:cursor-pointer">
-                        <img src={search} alt="search" className="md:mt-[0.35rem] xl:mt-2 lg:mt-2 mt-[0.35rem] md:ml-5 ml-[0.15rem] md:w-5 w-3"/>
+                        <img src={search} alt="search" className="md:mt-[0.35rem] xl:mt-2 lg:mt-2 mt-[0.35rem] md:ml-5 ml-[0.15rem] md:w-5 w-3" />
                     </div>
                 </div>
                 <div className="flex justify-between items-center">
-                    <img src={create} alt="create" className="md:block hidden xl:w-10 lg:w-10 md:w-7 w-4 md:mr-6 hover:cursor-pointer"/>
-                    <img src={notification} alt="notification" className="xl:w-7 lg:w-7 md:w-5 w-4 md:mr-6 mr-2 hover:cursor-pointer"/>
-                    <img src={user} alt="user" className="xl:w-[1.85rem] lg:w-[1.85rem] md:w-5 w-4 md:mr-6 mr-2 hover:cursor-pointer"/>
+                    <img src={create} alt="create" className="md:block hidden xl:w-10 lg:w-10 md:w-7 w-4 md:mr-6 hover:cursor-pointer" />
+                    <img src={notification} alt="notification" className="xl:w-7 lg:w-7 md:w-5 w-4 md:mr-6 mr-2 hover:cursor-pointer" />
+                    <img src={user} alt="user" className="xl:w-[1.85rem] lg:w-[1.85rem] md:w-5 w-4 md:mr-6 mr-2 hover:cursor-pointer" />
                 </div>
             </div>
-            {inputFocus&&<div className="bg-[#212121] z-50 shadow-lg border-[1px] border-[#393939] absolute left-[28.7rem] rounded-xl  w-[34.1rem] -mt-2">
+            {inputFocus && <div className="bg-[#212121] z-50 shadow-lg border-[1px] border-[#393939] absolute left-[28.7rem] rounded-xl  w-[34.1rem] -mt-2">
                 <ul className="py-4 px-3">
                     {searchData.map((i, index) => {
                         return <div className="flex hover:bg-[#474747] w-[34rem] hover:-ml-3 hover:pl-[0.85rem]"><img src={search} className="w-4 mt-2 ml-[0.15rem]" /><li key={index} className="px-2 py-1 text-white font-bold mt-1 ml-[0.35rem]">{i}</li></div>
@@ -87,6 +94,6 @@ const Header = () => {
                 </ul>
             </div>}
         </>
-    );    
+    );
 }
 export default Header;
