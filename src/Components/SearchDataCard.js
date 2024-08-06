720bef798e3d7581736497de2f30118e697d7af9
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-08-07 00:17:14 +0530

    styling for SearchDataCard

--- src/Components/SearchDataCard.js
+++ src/Components/SearchDataCard.js
@@ -96,15 +96,17 @@ const SearchDataCard = ({ data }) => {
     //   }
   return (
     <div>
-      <Link to={`/watch?v=${data?.id?.videoId}`}><div className='w-[24.2rem] h-[21rem] mb-6 hover:cursor-pointer' onClick={() => {
+      <Link to={`/watch?v=${data?.id?.videoId}`}><div className='flex w-[75.2rem] -mb-12 mt-4 h-[21rem] hover:cursor-pointer ml-16' onClick={() => {
         dispatch(apiResponse(data));
       }}>
-        <img src={data?.snippet?.thumbnails?.high?.url ? data?.snippet?.thumbnails?.high?.url : data?.snippet?.thumbnails?.medium?.url} alt='videoImage' className='rounded-lg w-[55rem] h-56' />
-        <div className='flex mt-3'>
-          <img src={data?.snippet?.thumbnails?.high?.url ? data?.snippet?.thumbnails?.high?.url : data?.snippet?.thumbnails?.medium?.url} alt='videoImage' className='rounded-full w-10 h-10' />
+        <img src={data?.snippet?.thumbnails?.high?.url ? data?.snippet?.thumbnails?.high?.url : data?.snippet?.thumbnails?.medium?.url} alt='videoImage' className='rounded-lg w-[30rem] h-72' />
+        <div className='mt-3'>
           <div className='ml-3'>
-            <p className='text-white w-[20rem] font-semibold text-lg'>{(data?.snippet?.title).length > 55 ? data?.snippet?.title.substr(0, 55) + "..." : data?.snippet?.title}</p>
-            <p className='text-[#AAAAAA] font-semibold'>{data?.snippet?.channelTitle.length > 32 ? data?.snippet?.channelTitle.substr(0, 32) : data?.snippet?.channelTitle}</p>
+            <p className='text-white w-full font-semibold text-lg'>{(data?.snippet?.title).length > 68 ? data?.snippet?.title.substr(0, 67) + "..." : data?.snippet?.title}</p>
+            <div className='flex mt-2'>
+          <img src={data?.snippet?.thumbnails?.high?.url ? data?.snippet?.thumbnails?.high?.url : data?.snippet?.thumbnails?.medium?.url} alt='videoImage' className='rounded-full w-8 h-8' />
+              <p className='text-[#AAAAAA] font-semibold mt-1 ml-1'>{data?.snippet?.channelTitle.length > 32 ? data?.snippet?.channelTitle.substr(0, 32) : data?.snippet?.channelTitle}</p>
+              </div>
             {/* <div className='text-[#AAAAAA] font-semibold flex'>
               <p>{formatViewCount(data?.statistics?.viewCount)} Views</p>
               <p className='text-2xl -mt-3 mx-1'>.</p>
