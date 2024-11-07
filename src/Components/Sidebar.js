1feec6510e4fc55adf062ae6cc6e76b4e0e75506
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-11-07 23:57:16 +0530

    creating portal for watch sidebar.

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,13 +2,15 @@ import React from 'react'
 import SmallSidebar from './SmallSidebar'
 import LargeSidebar from './LargeSidebar'
 import { useSelector } from 'react-redux'
+import WatchSidebar from './WatchSidebar'
 
 const Sidebar = () => {
   const toggle = useSelector((store) => store.Toggle.flag); 
   const sideBarsToggle = useSelector((store) => store.Toggle.sideBars);
   return (
     <div>
-      {toggle ? <LargeSidebar /> : sideBarsToggle&&<SmallSidebar />}
+      {sideBarsToggle==true&&toggle ? <LargeSidebar /> : sideBarsToggle && <SmallSidebar />}
+      {toggle&& sideBarsToggle==false&&<WatchSidebar/>}
     </div>
   )
 }
