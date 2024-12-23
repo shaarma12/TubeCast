0ce1406d6a5280d0e4523f911a60d70abe2a914e
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-12-23 21:36:20 +0530

    creating custom hook for searchData api

--- src/Components/SearchData.js
+++ src/Components/SearchData.js
@@ -1,27 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
-import { SUGGESTION_DATA_API } from '../constant';
-import SearchDataCard from './SearchDataCard';
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import SearchDataCard from "./SearchDataCard";
+import useSearchVideo from "../utils/useSearchVideo";
 
 const SearchData = () => {
-    const [searchParams] = useSearchParams();
-    let params = searchParams.get("search_query");
-    const [searchData, setSearchData] = useState([]);
-    useEffect(() => {
-        getSearchData();
-    }, [params]);
-    const getSearchData = async() => {
-        const data = await fetch(SUGGESTION_DATA_API + params);
-        const dataJson = await data.json();
-      setSearchData(dataJson?.items);
-    }
+  const [searchParams] = useSearchParams();
+  let params = searchParams.get("search_query");
+  console.log("params", params);
+  // calling the custom hook for search data.
+  const { searchData } = useSearchVideo({params});
+  console.log("SearchData", searchData);
   return (
-    <div className='h-[37rem] overflow-y-scroll no-scrollbar'>
-      {searchData.map((i,index) => {
-        return <SearchDataCard key={index} value={i} />
-          })}
+    <div className="h-[37rem] overflow-y-scroll no-scrollbar">
+      {searchData.map((i, index) => {
+        return <SearchDataCard key={index} value={i} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default SearchData
+export default SearchData;
---
+++ src/utils/useSearchVideo.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import { SUGGESTION_DATA_API } from '../constant';
+
+const useSearchVideo = ({ params }) => {
+  const [searchData, setSearchData] = useState([]);
+  useEffect(() => {
+    getSearchData();
+  }, [params]);
+    const getSearchData = async() => {
+      const data = await fetch(SUGGESTION_DATA_API + params);
+      const dataJson = await data.json();
+    setSearchData(dataJson?.items);
+  }
+  
+  return ({ searchData });
+}
+
+export default useSearchVideo
